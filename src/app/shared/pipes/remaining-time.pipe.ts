30d05a0c77093f98972f79fd488445a92ca6cd17
Gorgant CX1 <gorgant@gorgant.com>
2019-01-07 18:40:15 +0800

    implement custom theme injection

--- src/app/shared/pipes/remaining-time.pipe.ts
+++ src/app/shared/pipes/remaining-time.pipe.ts
@@ -8,7 +8,11 @@ export class RemainingTimePipe implements PipeTransform {
   transform(value: number): string {
     const days = Math.round((value / (1000 * 60 * 60 * 24)) % 7);
     const weeks = Math.round(value / (1000 * 60 * 60 * 24 * 7));
-    return `${weeks} Weeks ${days} Days Remaining`;
+    if (weeks > 0) {
+      return `${weeks} Weeks, ${days} Days`;
+    } else {
+      return `${days} Days`;
+    }
   }
 
 }
