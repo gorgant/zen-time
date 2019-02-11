b64baebeac401ca2f8dc670cb8106097205e41e8
Gorgant CX1 <gorgant@gorgant.com>
2019-02-11 12:49:25 +0800

    update logo to zentimer

--- src/app/shared/assets/imageUrls.ts
+++ src/app/shared/assets/imageUrls.ts
@@ -1,6 +1,6 @@
 export enum imageUrls {
   // tslint:disable-next-line:max-line-length
-  ZEN_TIMER_LOGO = 'https://firebasestorage.googleapis.com/v0/b/gorgant-zentime.appspot.com/o/graphics%2Fzen_time_logo_2.svg?alt=media&token=7749b804-1e59-473e-b113-97b4d75324ee',
+  ZEN_TIMER_LOGO = 'https://firebasestorage.googleapis.com/v0/b/gorgant-zentime.appspot.com/o/graphics%2Fcore-app-images%2Fzen_time_logo_3.svg?alt=media&token=c368032b-59e7-4c2a-8b77-06a8d7cf85ca',
   // tslint:disable-next-line:max-line-length
   PROFILE_DEFAULT_IMAGE = 'https://firebasestorage.googleapis.com/v0/b/gorgant-zentime.appspot.com/o/graphics%2Fbaseline-person-24px.svg?alt=media&token=a4d6fbb7-b000-41ce-98da-190b03839bd4',
   // tslint:disable-next-line:max-line-length
